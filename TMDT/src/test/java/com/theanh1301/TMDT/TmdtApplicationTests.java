43e7756b4b756adb9fdaba60fb9b22c9712c5a7b
Theanh130124 <theanhtran13012004@gmail.com>
2025-06-09 19:33:45 +0700

    Get started

---
+++ TMDT/src/test/java/com/theanh1301/TMDT/TmdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theanh1301.TMDT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
